3d3079e0d6560c3803a21cba4e75ee0f19115401
Hang924 <1253285172@qq.com>
2023-01-06 15:23:16 +0800

    修改和数据库表映射错误BUG

--- src/main/java/org/ecust/system/pojo/entity/Course.java
+++ src/main/java/org/ecust/system/pojo/entity/Course.java
@@ -1,13 +1,16 @@
 package org.ecust.system.pojo.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 @Data
 public class Course{
     private Long id;
+    @TableField("courseNo")
     private Long courseNo;
     private String name;
     private float credit;
     private Long term;
+    @TableField("teacherName")
     private String teacherName;
 }
