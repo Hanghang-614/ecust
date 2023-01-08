d5f3ed07b444788f9c4880e378202786d5942724
Hang924 <1253285172@qq.com>
2023-01-08 14:19:53 +0800

    跨域配置

---
+++ src/main/java/org/ecust/system/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package org.ecust.system.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //跨域配置，不可设置为*，不安全, 前后端分离项目，可能域名不一致
+        //本地测试 端口不一致 也算跨域
+//        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+}
